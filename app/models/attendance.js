ea4cd8ad1aa4a9568fef28af371aad2dbf623bf4
Hazem Khaled <hazem.khaled@gmail.com>
2015-09-05 15:56:01 +0200

    Rename model, now starting with lower case

--- app/models/attendance.js
+++ app/models/attendance.js
@@ -1,37 +1,37 @@
 exports.definition = {
 	config: {
 		columns: {
-		    "studentId": "int",
-		    "instractorId": "int",
-		    "date": "string",
-		    "note": "string"
+			"studentId": "int",
+			"instractorId": "int",
+			"date": "string",
+			"note": "string"
 		},
 		adapter: {
 			type: "sql",
 			collection_name: "attendance"
 		}
 	},
-	extendModel: function(Model) {
-		_.extend(Model.prototype, {
+	extendModel: function( Model ) {
+		_.extend( Model.prototype, {
 			// extended functions and properties go here
-		});
+		} );
 
 		return Model;
 	},
-	extendCollection: function(Collection) {
-		_.extend(Collection.prototype, {
+	extendCollection: function( Collection ) {
+		_.extend( Collection.prototype, {
 			// extended functions and properties go here
 
 			// For Backbone v1.1.2, uncomment the following to override the
 			// fetch method to account for a breaking change in Backbone.
 			/*
-			fetch: function(options) {
-				options = options ? _.clone(options) : {};
-				options.reset = true;
-				return Backbone.Collection.prototype.fetch.call(this, options);
-			}
-			*/
-		});
+			 fetch: function(options) {
+			 options = options ? _.clone(options) : {};
+			 options.reset = true;
+			 return Backbone.Collection.prototype.fetch.call(this, options);
+			 }
+			 */
+		} );
 
 		return Collection;
 	}
