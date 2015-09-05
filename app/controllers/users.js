5fb1ca98983c70bd0ffa01630b5d71562901c5c4
Hazem Khaled <hazem.khaled@gmail.com>
2015-09-05 19:01:39 +0200

    Start implementing item click

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,7 +1,25 @@
 function transfomer( model ) {
 	var transform = model.toJSON( );
-	transform.balance = "Balance : " + Math.floor( 600 + Math.random( ) * 0 ) + "/600";
+	transform.balance = "Balance : " + Math.floor( Math.random( ) * ( 600 - 400 ) + 400 ) + "/600";
 	return transform;
 }
 
-Alloy.Collections.user.fetch( ); 
+Alloy.Collections.user.fetch( );
+
+//Alloy.Collections.user.models.length;
+
+function setActionBar( ) {
+	$.users.activity.actionBar.subtitle = "We have " + Alloy.Collections.user.models.length + " user";
+}
+
+function openReport( e ) {
+	// get the clicked item from that section
+	var item = $.list.sections[ 0 ].getItemAt( e.itemIndex );
+
+	// clicked button
+	// alert(e.bindId);
+}
+
+function closeMe () {
+	$.users.close();
+}
